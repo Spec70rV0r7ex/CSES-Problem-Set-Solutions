bdb72b138e73e07a9d3e311bba9104396d28ffcf
Spec70rV0r7ex <5pec70rv0r7ex4321@gmail.com>
2026-06-18 01:38:58 +0530

    Add Bit_Strings.cpp for binary exponentiation

---
+++ Introductory Problems/Bit_Strings.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+int MOD = 1e9 + 7;
+
+// iterative method of binary exponentiation of a ^ b mod N
+int power (int a, int b, int N) {
+    int ans = 1;
+    while(b) {
+        if(b & 1LL) {
+            ans = (ans * a) % N;
+        }
+        a = (a * a) % N;
+        b >>= 1LL;
+    }
+    return ans;
+}
+
+int32_t main() {
+    int n;
+    cin >> n;
+    cout << power(2LL, n, MOD) << '\n';
+    return 0;
+}
