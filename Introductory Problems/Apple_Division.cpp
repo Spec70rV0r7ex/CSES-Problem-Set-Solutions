4e44f833266e4c2fbb73997f9f5027c1ad6a9ee9
Spec70rV0r7ex <5pec70rv0r7ex4321@gmail.com>
2026-06-29 12:45:24 +0530

    Refactor func parameters and update main logic

--- Introductory Problems/Apple_Division.cpp
+++ Introductory Problems/Apple_Division.cpp
@@ -10,28 +10,28 @@ istream& operator>>(istream& in, vector<T>& v) {
     return in;
 }
 
-void func(int n, vector<int>& p, int i, int& m, int x) {
+void func(int n, vector<int>& p, int i, int x, int& ans) {
     if (i == n) { 
         return;
     }
     x += 2 * p[i];
-    m = ((x >= 0) ? min(m, x) : m);
-    func(n, p, i + 1, m, x);
+    ans = ((x >= 0) ? min(ans, x) : ans);
+    func(n, p, i + 1, x, ans);
     x -= 2 * p[i];
-    func(n, p, i + 1, m, x);
+    func(n, p, i + 1, x, ans);
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m = LLONG_MAX;
+    int n, ans = LLONG_MAX;
     cin >> n;
     vector<int> p(n, 0LL);
     cin >> p;
 
-    func(n, p, 0LL, m, -accumulate(all(p), 0LL));
-    cout << m << '\n';
+    func(n, p, 0LL, -accumulate(all(p), 0LL), ans);
+    cout << ans << '\n';
 
     return 0;
 }
